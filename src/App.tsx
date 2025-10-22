61e25c393cbea8f78b892ecb626e0298d70d0184
Raj-Venkat-R <rajvenkat.r361@gmail.com>
2025-10-22 23:53:23 +0530

    Fix base path for root domain GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/green-cloud-vision">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
